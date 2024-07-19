309146de1c65a50f264d3cf8b8e53b0a350e9de7
Yehonatan <yehonatanrefael@gmail.com>
2024-07-19 15:00:25 +0300

    chore: Update primary color and font family in theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,6 @@
 import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  primaryColor: "#FF3E00",
+  fontFamily: "Arial",
 });
