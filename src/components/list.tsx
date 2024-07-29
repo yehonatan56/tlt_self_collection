7b69beeb49c181b7b1021a83677e04a6f9781f35
Yehonatan <yehonatanrefael@gmail.com>
2024-07-29 16:32:45 +0300

    chore: Add react-responsive-pagination package and implement pagination in List component.

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,22 +1,30 @@
 import { useEffect, useState } from "react";
 import { remult } from "remult";
 import { Item } from "../sherd/item";
-import { Checkbox, Table } from "@mantine/core";
+import { Table } from "@mantine/core";
 import ListItem from "./item";
+import ResponsivePaginationComponent from "react-responsive-pagination";
+import "react-responsive-pagination/themes/classic.css";
+
 const repo = remult.repo(Item);
-const fetchItems = async () => {
-  return await repo.find({ limit: 10 });
+const fetchItems = async (page: number) => {
+  return await repo.find({ limit: 10, page });
 };
 export default function List() {
   const [items, setItems] = useState<Item[]>([]);
-  const [checked, setChecked] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
   useEffect(() => {
-    fetchItems().then((items) => setItems(items));
-  }, []);
+    fetchItems(currentPage).then((items) => setItems(items));
+  }, [currentPage]);
 
   return (
     <div>
       <h1>רשימת פריטים</h1>
+      <ResponsivePaginationComponent
+        current={currentPage}
+        total={10}
+        onPageChange={setCurrentPage}
+      />
       <Table border={2}>
         <Table.Thead>
           <Table.Tr>
