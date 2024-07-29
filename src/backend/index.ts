9d72394d2e2203bc44b02be7b83522f11f357a97
Yehonatan <yehonatanrefael@gmail.com>
2024-07-29 19:48:11 +0300

    chore: Update npm dependencies and add PostgreSQL support
    - Update npm dependencies to latest stable versions
    - Add PostgreSQL support with the pg package
    - Update TypeScript type definitions for pg package
    
    ```

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -3,11 +3,19 @@ import { remultExpress } from "remult/remult-express";
 import { Item } from "../sherd/item";
 import { whatsapp } from "./whatsapp";
 import { startWhatsapp } from "./whatsapp-on-server";
+import Pool from "pg";
+import { SqlDatabase } from "remult";
+import { PostgresDataProvider } from "remult/postgres";
+const pg = new Pool.Pool({
+  connectionString: "postgres://postgres:102030@localhost:5432/postgres",
+});
 const app = express();
 const PORT = 3000;
 
 app.use(
   remultExpress({
+    dataProvider: new SqlDatabase(new PostgresDataProvider(pg)),
+
     controllers: [whatsapp],
     entities: [Item],
   })
